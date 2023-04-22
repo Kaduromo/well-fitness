738af5699a6244220e31efcb77d91305390b12f8
Kaduromo <vitalzsk@gmail.com>
2023-04-22 22:57:37 +0300

    first commit

---
+++ src/components/Layout.tsx
@@ -0,0 +1,14 @@
+import Header from "./Header"
+import Footer from "./Footer"
+
+const Layout = () => {
+  return (
+    <div>
+      <Header />
+      Layout
+      <Footer />
+    </div>
+  )
+}
+
+export default Layout
